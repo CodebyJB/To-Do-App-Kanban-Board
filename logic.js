2ed0112e75ff01b31d89393b8240a1ab495afb1f
Jennifer <106908910+JenniferBraunbarth@users.noreply.github.com>
2023-01-16 14:54:54 +0100

    Fourth Commit

--- logic.js
+++ logic.js
@@ -7,29 +7,61 @@ const addBtn = document.getElementById("add_btn");
 
 const toDoContainer = document.querySelector(".to_do");
 
-const insertTaskContainer = (titi) => {
-  const deleteTask = () => {
-    newTaskContainer.remove();
-  };
+const todos = [];
+let date, priority, task;
 
+const sortTodos = () => {
+  todos.sort((a, b) => {
+    return new Date(a.date) - new Date(b.date);
+  });
+};
+
+const columns = Array.from(document.querySelectorAll(".columns"));
+columns.forEach((column) => {
+  column.addEventListener("dragover", dragOver);
+  column.addEventListener("drop", dragDrop);
+});
+let dragItem = null;
+
+function dragStart() {
+  dragItem = this;
+}
+
+function dragEnd() {
+  //   this.className = "to_do_container";
+  dragItem = null;
+}
+
+function dragDrop() {
+  this.append(dragItem);
+  console.log(this);
+  console.log(this.children);
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+const insertTaskContainer = (dueDate, colorPriority, todoTask) => {
   const newTaskContainer = document.createElement("div");
   newTaskContainer.classList.add("to_do_container");
   toDoContainer.append(newTaskContainer);
-  if (color.value === "high") {
+  newTaskContainer.setAttribute("draggable", "true");
+  if (colorPriority === "high") {
     newTaskContainer.style.borderTop = "1rem solid #ff65a3";
-  } else if (color.value === "med") {
+  } else if (colorPriority === "med") {
     newTaskContainer.style.borderTop = "1rem solid #fff740";
-  } else if (color.value === "soso") {
+  } else if (colorPriority === "soso") {
     newTaskContainer.style.borderTop = "1rem solid #7afcff";
   }
-  todo.priority = color.value;
+  priority = colorPriority;
+  newTaskContainer.addEventListener("dragstart", dragStart);
+  newTaskContainer.addEventListener("dragend", dragEnd);
 
   const newTaskInput = document.createElement("p");
   newTaskInput.setAttribute("contenteditable", "true");
   newTaskContainer.append(newTaskInput);
-  //   newTaskInput.innerText = taskInput.value;
-  newTaskInput.innerText = titi;
-  todo.task = newTaskInput.innerText;
+  newTaskInput.innerText = todoTask;
+  task = newTaskInput.innerText;
 
   const newTaskFooter = document.createElement("div");
   newTaskFooter.classList.add("task_foot");
@@ -38,24 +70,27 @@ const insertTaskContainer = (titi) => {
   const dateSelected = document.createElement("p");
   dateSelected.classList.add("date");
   newTaskFooter.append(dateSelected);
-  todo.date = dateInput.value;
+  date = dueDate;
 
   const today = new Date();
   const tomorrow = new Date(today);
   tomorrow.setDate(tomorrow.getDate() + 1);
-  if (
-    today.toLocaleDateString() ===
-    new Date(dateInput.value).toLocaleDateString()
-  ) {
+  if (today.toLocaleDateString() === new Date(dueDate).toLocaleDateString()) {
     dateSelected.innerText = "today";
   } else if (
-    tomorrow.toLocaleDateString() ===
-    new Date(dateInput.value).toLocaleDateString()
+    tomorrow.toLocaleDateString() === new Date(dueDate).toLocaleDateString()
   ) {
     dateSelected.innerText = "tomorrow";
   } else {
-    dateSelected.innerText = dateInput.value;
+    dateSelected.innerText = dueDate;
   }
+
+  const deleteTask = () => {
+    const index = todos.findIndex((obj) => obj.task === todoTask);
+    todos.splice(index, 1);
+    newTaskContainer.remove();
+    localStorage.setItem("todo_list", JSON.stringify(todos));
+  };
 
   const deleteBtn = document.createElement("p");
   deleteBtn.classList.add("delete_btn");
@@ -63,42 +98,34 @@ const insertTaskContainer = (titi) => {
   deleteBtn.innerText = "🗑️❌";
   deleteBtn.addEventListener("click", deleteTask);
 };
-
 const addTask = addBtn.addEventListener("click", () => {
-  insertTaskContainer(taskInput.value);
-  todos.push(todo);
-  //   localStorage.setItem("task", todo);
+  insertTaskContainer(dateInput.value, color.value, taskInput.value);
+  todos.push({ date, priority, task });
+  sortTodos();
   setItems();
   taskInput.value = "";
   dateInput.value = "";
   color.value = "";
+  location.reload();
 });
 
-const todos = [];
-console.log(todos);
-const todo = {};
-console.log(todo);
-
-// const getItems = () => {
-//   const value = localStorage.getItem("todo-test") || [];
-//   return JSON.parse(value);
-// };
-
-// let items = getItems();
-
 const setItems = () => {
-  const itemsJson = JSON.stringify(todos);
-  localStorage.setItem("todo-test", itemsJson);
+  window.localStorage.setItem("todo_list", JSON.stringify(todos));
 };
-
-// On app load, get all tasks from localStorage
 
 const loadTasks = () => {
-  const value = localStorage.getItem("todo-test") || [];
-  // Get the tasks from localStorage and convert it to an array
-  let tasks = Array.from(JSON.parse(localStorage.getItem("todo-test")));
-  console.log(tasks);
-  insertTaskContainer(taskInput.value);
+  let tasks = Array.from(
+    JSON.parse(window.localStorage.getItem("todo_list")) || []
+  );
+  todos.push(...tasks);
+
+  todos.map((item) => {
+    insertTaskContainer(item.date, item.priority, item.task);
+  });
+
+  sortTodos();
 };
+console.log(todos);
 
 window.onload = loadTasks();
+// localStorage.clear();
